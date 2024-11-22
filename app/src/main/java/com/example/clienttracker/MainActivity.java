f4b0a2112ef481e88cff13be00e88b802b059e7f
Pavithra-Djanaguiraman <pavithra.djanaguiraman190@ptuniv.edu.in>
2024-11-22 22:04:06 +0530

    Launch and login page ui

--- app/src/main/java/com/example/clienttracker/MainActivity.java
+++ app/src/main/java/com/example/clienttracker/MainActivity.java
@@ -1,24 +1,86 @@
 package com.example.clienttracker;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.UnderlineSpan;
+import android.text.TextPaint;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+         //Find Views for Buttons
+        CardView btnSignUpGoogle = findViewById(R.id.btn_sign_up_google);
+        CardView btnSignUpNumber = findViewById(R.id.btn_sign_up_number);
+
+
+        TextView loginTextView = findViewById(R.id.tv_login);
+
+        // Set onClickListener for Google Sign-Up Button
+        btnSignUpGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to Google Sign-In page
+                Intent intent = new Intent(MainActivity.this, GoogleSignInActivity.class);
+                startActivity(intent);
+            }
         });
+
+        // Set onClickListener for Number Sign-Up Button
+        btnSignUpNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to Number Sign-In page
+                Intent intent = new Intent(MainActivity.this, NumberSignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Set ClickableSpan for "Login" hyperlink
+        String text = "Already have an account? Login";
+        SpannableString spannableString = new SpannableString(text);
+
+        // Find the start and end index of the "Login" word
+        int startIndex = text.indexOf("Login");
+        int endIndex = startIndex + "Login".length();
+
+        // Create a ClickableSpan for "Login"
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                // Navigate to Login page
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE); // Set hyperlink color
+                ds.setUnderlineText(true); // Add underline to the text
+            }
+        };
+
+         //Apply the ClickableSpan to the "Login" word
+        spannableString.setSpan(clickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        loginTextView.setText(spannableString);
+        loginTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+
     }
 }
